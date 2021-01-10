d530bcbca98808555146270dc82f93153226a0dd
Naoki Nakamichi <ronnao0516@gmail.com>
2021-01-10 14:23:40 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const target = document.getElementById("mitarashi"); // アニメーションの対象
+const startX = 50; // X座標の始点
+const startY = 50; // Y座標の始点
+const endX = 500; // X座標の目標点
+const endY = 150; // Y座標の目標点
+const diffX = endX - startX; // X座標の変化量
+const diffY = endY - startY; // Y座標の変化量
+const time = 1000; // 変化にかける時間 (ミリ秒)
