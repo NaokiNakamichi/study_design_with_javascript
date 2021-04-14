ed723425d8d743d99400fd721a452f672b353c44
Naoki Nakamichi <ronnao0516@gmail.com>
2021-04-15 01:06:54 +0900

    pp

---
+++ logo.js
@@ -0,0 +1,40 @@
+
+$(function() {
+    winW = $(window).width();
+    spped = 2000;
+    $('#horizon1').animate({
+        width: winW
+    }, spped);
+    
+    $('#horizon2').animate({
+        width: winW
+    }, spped);
+    $('#horizon3').animate({
+        width: winW
+    }, spped);
+    $('#horizon4').animate({
+        width: winW
+    }, spped);
+    $('#horizon5').animate({
+        width: winW
+    }, spped);
+
+    $('#horizon6').animate({
+        width: winW
+    }, spped);
+    $('#horizon7').animate({
+        width: winW
+    }, spped);
+    $('#horizon8').animate({
+        width: winW
+    }, spped);
+    $('#horizon9').animate({
+        width: winW
+    }, spped);
+    $('#horizon10').animate({
+        width: winW
+    }, spped);
+    $('#horizon11').animate({
+        width: winW
+    }, spped);
+});
