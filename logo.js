b434f17e7069ded18bd3ef6e407b2bfc9e7aad64
Naoki Nakamichi <ronnao0516@gmail.com>
2021-04-15 13:30:41 +0900

    hmmmm

--- logo.js
+++ logo.js
@@ -1,40 +1,23 @@
 
-$(function() {
+function Horizon(tmp) {
     winW = $(window).width() * 2;
     spped = 2000;
-    $('#horizon1').animate({
+    $(tmp).animate({
         width: winW
     }, spped);
-    
-    $('#horizon2').animate({
-        width: winW
-    }, spped);
-    $('#horizon3').animate({
-        width: winW
-    }, spped);
-    $('#horizon4').animate({
-        width: winW
-    }, spped);
-    $('#horizon5').animate({
-        width: winW
-    }, spped);
+  }
 
-    $('#horizon6').animate({
-        width: winW
-    }, spped);
-    $('#horizon7').animate({
-        width: winW
-    }, spped);
-    $('#horizon8').animate({
-        width: winW
-    }, spped);
-    $('#horizon9').animate({
-        width: winW
-    }, spped);
-    $('#horizon10').animate({
-        width: winW
-    }, spped);
-    $('#horizon11').animate({
-        width: winW
-    }, spped);
-});
+$(window).scroll(function(){
+    line = "#horizon1"
+    var top = $(line).offset().top; // ターゲットの位置取得
+    var position = top - $(window).height();  // 発火させたい位置
+    if ($(window).scrollTop() > position) {
+        for (let i = 1; i < 12; i++) {
+            line = "#horizon" + i
+            Horizon(line)
+          }
+      }
+    
+
+    
+  })
